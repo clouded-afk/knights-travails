f140f355618f00e069630d305a411f81f4b5dcaf
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-10-22 18:13:32 -0400

    Final commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { knightMoves } from "./knights-travails.js";
 
-knightMoves([3,8], [4,3])
+knightMoves([3,3], [4,3])
 knightMoves([7,2], [2,5])
 knightMoves([0,0], [7,7])
 knightMoves([2,8], [4,7])

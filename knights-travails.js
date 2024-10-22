779ad7767eab6840791c4dcd1a0f051062f5560d
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-10-22 16:17:39 -0400

    Add conditional to knightsMOves for when the start position equals the end position

--- knights-travails.js
+++ knights-travails.js
@@ -1,5 +1,7 @@
 function knightMoves(startPosition, endPosition) {
-
+    if (startPosition[0] === endPosition[0] && startPosition[1] === endPosition[1]) {
+        return 'You made it in 0 moves!'
+    }
 }
 
 export {

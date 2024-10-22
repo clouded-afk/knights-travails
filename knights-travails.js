c8f222d320ef425a92f1ef4c36d1f378e300a9bc
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-10-22 17:01:10 -0400

    Edit function to find shortest path from start to end

--- knights-travails.js
+++ knights-travails.js
@@ -10,11 +10,32 @@ const potentialMoves = [
 ]
 
 function knightMoves(startPosition, endPosition) {
-    if (startPosition[0] === endPosition[0] && startPosition[1] === endPosition[1]) {
-        return 'You made it in 0 moves!'
+    const queue = [[startPosition, [startPosition]]]
+    const visited = new Set()
+
+    visited.add(startPosition.toString())
+
+    while (queue.length > 0) {
+        const [current, path] = queue.shift()
+
+        if (current[0] === endPosition[0] && current[1] === endPosition[1]) {
+            console.log(`You made it in ${path.length - 1} moves!`)
+            path.forEach(square => {
+                console.log(square)
+            });
+            return
+        }
+
+        for (const [dx, dy] of potentialMoves) {
+            const next = [current[0] + dx, current[1] + dy]
+            if (next[0] >= 0 && next[0] < 8 && next[1] >= 0 && next[1] < 8 && !visited.has(next.toString())) {
+                visited.add(next.toString())
+                queue.push([next, [...path, next]])
+            }
+            
+        }
     }
 }
-
 
 
 export {

df181e3344fec470efcd186185d762ac50db36ac
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-10-22 16:29:22 -0400

    Add an array which contains all possible moves from a starting position

--- knights-travails.js
+++ knights-travails.js
@@ -1,8 +1,21 @@
+const potentialMoves = [
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1],
+    [1, 2],
+    [1, -2],
+    [-1, 2],
+    [-1, -2]
+]
+
 function knightMoves(startPosition, endPosition) {
     if (startPosition[0] === endPosition[0] && startPosition[1] === endPosition[1]) {
         return 'You made it in 0 moves!'
     }
 }
+
+
 
 export {
     knightMoves

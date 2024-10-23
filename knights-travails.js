dd14b5a4e3292b9fa4e7569522ffd75e98058d3a
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-10-23 16:12:35 -0400

    Refactor knightMoves

--- knights-travails.js
+++ knights-travails.js
@@ -1,48 +1,47 @@
-const potentialMoves = [
-    [2, 1],
-    [2, -1],
-    [-2, 1],
-    [-2, -1],
-    [1, 2],
-    [1, -2],
-    [-1, 2],
-    [-1, -2]
-]
-
 function knightMoves(startPosition, endPosition) {
-    const queue = [[startPosition, [startPosition]]]
-    const visited = new Set()
+    const BOARDSIZE = 8
 
-    visited.add(startPosition.toString())
+    const possibleKinghtMoves = [
+        [2,1],
+        [2,-1],
+        [-2,1],
+        [-2,-1],
+        [1,2],
+        [1,-2],
+        [-1,2],
+        [-1,-2],
+    ]
 
-    if (startPosition[0] > 7 || startPosition[0] < 0 || startPosition[1] > 7 || startPosition[1] < 0 || endPosition[0] > 7 || endPosition[0] < 0 || endPosition[1] > 7 || endPosition[1] < 0) {
-        console.log("Start or End Position coordinates are invalid, ensure all points are between 0 - 7!")
-        return
+    function validatePosition(x, y) {
+        return x >= 0 && x < BOARDSIZE && y >= 0 && y < BOARDSIZE
     }
 
-    while (queue.length > 0) {
-        const [current, path] = queue.shift()
+    for (const point of [...startPosition, ...endPosition]) {
+        if (point >= BOARDSIZE || point < 0) {
+            throw new Error('Invalid Start or End position. The board is an 8x8, all points must be in the range 0-7.')
+        }
+    }
 
-        if (current[0] === endPosition[0] && current[1] === endPosition[1]) {
-            console.log(`You made it in ${path.length - 1} moves!`)
-            path.forEach(square => {
-                console.log(square)
-            });
-            return
+    const queue = [[startPosition, [startPosition]]]
+    const visitedBoardSquares = new Set()
+
+    while (queue.length > 0) {
+        const [vertex, path] = queue.shift()
+
+        if (vertex[0] === endPosition[0] && vertex[1] === endPosition[1]) {
+            return path
         }
 
-        for (const [dx, dy] of potentialMoves) {
-            const next = [current[0] + dx, current[1] + dy]
-            if (validatePosition(next[0], next[1]) && !visited.has(next.toString())) {
-                visited.add(next.toString())
-                queue.push([next, [...path, next]])
+        for (const move of possibleKinghtMoves) {
+            const nextMove = [vertex[0] + move[0], vertex[1] + move[1]]
+
+            if (validatePosition(nextMove[0], nextMove[1]) && !visitedBoardSquares.has(nextMove.toString())) {
+                visitedBoardSquares.add(nextMove.toString())
+                queue.push([nextMove, [...path, nextMove]])
             }
         }
     }
-}
-
-function validatePosition(x, y) {
-    return x >= 0 && x < 8 && y >= 0 && y < 8
+    return null
 }
 
 

a6cada6f731514d5a67d2dc4cf0643ba05016a09
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-10-23 16:12:59 -0400

    Add function to print out the result fo the knightMoves function

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 import { knightMoves } from "./knights-travails.js";
 
-knightMoves([3,3], [4,3])
-knightMoves([7,2], [2,5])
-knightMoves([0,0], [7,7])
-knightMoves([2,8], [4,7])
+function prettyPrint(startPosition, endPosition) {
+    try {
+        const result = knightMoves(startPosition, endPosition)
+
+        if (result === null) {
+            console.log(`No path between ${startPosition} --> ${endPosition}`)
+            return
+        }
+
+        console.log(`You made it in ${result.length - 1} ${result.length - 1 === 1 ? 'move' : 'moves'}! Here's your path from ${startPosition} --> ${endPosition}`)
+
+        for (const position of result) {
+            console.log(position)
+        }
+    } catch(e) {
+        console.log(e.message)
+    }
+}
+
+prettyPrint([3,3], [4,3])
+prettyPrint([7,2], [2,5])
+prettyPrint([0,0], [7,7])
+prettyPrint([2,8], [4,7])
